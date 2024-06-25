97336c0bdad787f9e209ca3c52edcb302d403679
adem öcel <ademocel83@gmail.com>
2024-06-25 17:06:39 +0300

    update 0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 
 setup(
     name='wiseman_library',
-    version='0.0.2',
+    version='0.0.3',
     author='adem öcel',
     author_email='ademocel83@gmail.com',
     description='wiseman bundles the voice assistant infrastructure into a python library',
